47214d557c71566590c88ee64158aa5ab08201e4
carmengo01 <carmela050701@gmail.com>
2022-03-04 19:48:31 +0100

    Fibonacci.java fixed

--- src/main/java/org/fibonacci/fibonacci/Fibonacci.java
+++ src/main/java/org/fibonacci/fibonacci/Fibonacci.java
@@ -2,7 +2,22 @@ package org.fibonacci.fibonacci;
 
 public class Fibonacci {
 
-    public int compute(int n){
-        return 0;
+    public int compute(int value){
+        if(value<0){
+            throw new RuntimeException("Error: value negative");
+        }
+        int result=0;
+        if(value==1){
+            result=1;
+        }else{
+            int minusTwo=0;
+            int minusOne=1;
+            for(int i=1; i<value; i++){
+                result=minusOne+minusTwo;
+                minusTwo=minusOne;
+                minusOne=result;
+            }
+        }
+        return result;
     }
 }
